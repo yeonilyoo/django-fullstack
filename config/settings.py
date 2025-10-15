dd23d110aee2858e89e97c287d2aa670a2233856
unknown <yeonilyoo@gmail.com>
2025-10-15 14:36:15 +0900

    clean up mail change

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-from common.const import EMAIL_USER, EMAIL_PASSWORD
+from common.const.const import EMAIL_USER, EMAIL_PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
