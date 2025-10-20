81da1cb33b48658d66b2cb780fc7aeb50085abeb
unknown <yeonilyoo@gmail.com>
2025-10-20 09:27:05 +0900

    reset database

--- QnA/apps.py
+++ QnA/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class QnAConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'QnA'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "QnA"
