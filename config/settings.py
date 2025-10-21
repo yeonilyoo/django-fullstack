4daeee2e1b78b5cd25dae88aa47bbba27c9b16bd
unknown <yeonilyoo@gmail.com>
2025-10-21 10:40:25 +0900

    added docker/db config

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 from config.const.const import EMAIL_USER, EMAIL_PASSWORD
@@ -98,8 +99,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",  # change from sqlite3
+        "NAME": os.environ.get("MYSQL_DATABASE", "mysite_db"),
+        "USER": os.environ.get("MYSQL_USER", "mysite_user"),
+        "PASSWORD": os.environ.get("MYSQL_PASSWORD", "mysite_pass"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "3306"),
     }
 }
 
