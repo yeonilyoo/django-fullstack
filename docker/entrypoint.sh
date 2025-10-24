d1e1d4fb3d09c3fcf4b997d50f458aa3cc321307
unknown <yeonilyoo@gmail.com>
2025-10-24 14:40:07 +0900

    fixed issues with docker deployment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,6 +8,8 @@
 #done
 #echo "MariaDB is ready"
 
+#since python slim doesn't include mysqlclient
+apt-get update && apt-get install -y default-libmysqlclient-dev build-essential && rm -rf /var/lib/apt/lists/*
 
 python manage.py migrate
 python manage.py collectstatic --noinput
