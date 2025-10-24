0969f30600012dca0b00667572fa06bab9eeb61e
unknown <yeonilyoo@gmail.com>
2025-10-24 12:42:12 +0900

    docker update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+#replaced with docker compose healthy check
 # Wait for MariaDB to be ready
-echo "Waiting for MariaDB to be ready..."
-until nc -z db 3306; do
-  sleep 1
-done
-echo "MariaDB is ready"
+#echo "Waiting for MariaDB to be ready..."
+#until nc -z db 3306; do
+#  sleep 1
+#done
+#echo "MariaDB is ready"
 
 
 python manage.py migrate
